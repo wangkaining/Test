174def9ee81093cf5e1c5ac21d4c4718f065ec26
wangkaining <185055346@qq.com>
2017-04-05 16:35:30 +0800

    Initial commit

---
+++ src/main/java/SuperClass.java
@@ -0,0 +1,22 @@
+import com.sun.org.glassfish.gmbal.IncludeSubclass;
+
+/**
+ * Created by 王凯宁 on 2017/4/5.
+ */
+public class SuperClass {
+    int a ;
+    public void in() {
+
+    }
+}
+
+class SubClass extends SuperClass {
+    int b ;
+    public void setB() {
+
+    }
+
+}
+
+
+
